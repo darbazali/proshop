3d7728f4a484730bd7ccadfa015a2e570b6606b7
Darbaz Ali <darbaz.a.h@gmail.com>
2020-10-14 19:45:17 +0300

    create Rating component

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -5,10 +5,10 @@ import Product from '../components/Product'
 const HomeScreen = () => {
     return (
         <>
-            <h1>Latest Products</h1>]
+            <h1>Latest Products</h1>
             <Row>
                 {products.map((product) => (
-                    <Col ms={12} md={6} lg={4}>
+                    <Col key={product._id} ms={12} md={6} lg={4}>
                         {/* <h3>{product.name}</h3> */}
                         <Product product={product} />
                     </Col>
