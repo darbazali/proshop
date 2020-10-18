fbc09a498092ec322165c42dfc25273bc4592553
Darbaz Ali <darbaz.me@gmail.com>
2020-10-18 19:18:34 +0300

    create store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,11 +4,13 @@ import './bootstrap.min.css'
 import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
+import { Provider } from 'react-redux'
+import store from './store.js'
 
 ReactDOM.render(
-    <React.StrictMode>
+    <Provider store={store}>
         <App />
-    </React.StrictMode>,
+    </Provider>,
     document.getElementById('root')
 )
 
---
+++ frontend/src/store.js
@@ -0,0 +1,14 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
+const reducer = combineReducers({})
+const initialState = {}
+const middlware = [thunk]
+const store = createStore(
+    reducer,
+    initialState,
+    composeWithDevTools(applyMiddleware(...middlware))
+)
+
+export default store
