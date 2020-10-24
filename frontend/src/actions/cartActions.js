ae4623e98d30a09e4a2589d9f1248d65eefb1ab1
Darbaz Ali <darbaz.me@gmail.com>
2020-10-24 15:41:35 +0300

    setup Cart screen

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,24 +1,21 @@
-import axios from 'axios'
-import { CART_ADD_ITEM } from '../constants/cartConstants'
+import axios from "axios"
+import { CART_ADD_ITEM } from "../constants/cartConstants"
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
-	const { data } = await axios.get(`/api/products/${id}`)
+  const { data } = await axios.get(`/api/products/${id}`)
 
-	dispatch({
-		type: CART_ADD_ITEM,
-		payload: {
-			product: data._id,
-			name: data.name,
-			image: data.image,
-			price: data.price,
-			contInStock: data.contInStock,
-			qty,
-		},
-	})
+  dispatch({
+    type: CART_ADD_ITEM,
+    payload: {
+      product: data._id,
+      name: data.name,
+      image: data.image,
+      price: data.price,
+      contInStock: data.contInStock,
+      qty,
+    },
+  })
 
-	// save items that been added to cart, in local storage
-	localStorage.setItem(
-		'cartItems',
-		JSON.stringify(getState().cart.cartItems)
-	)
+  // save items that been added to cart, in local storage
+  localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems))
 }
--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,31 +1,27 @@
-import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants'
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants"
 
 export const cartReducer = (state = { cartItems: [] }, action) => {
-	switch (action.type) {
-		case CART_ADD_ITEM:
-			const item = action.payload
-			const existItem = state.cartItems.find(
-				(x) => x.product === item.product
-			)
+  switch (action.type) {
+    case CART_ADD_ITEM:
+      const item = action.payload
+      const existItem = state.cartItems.find((x) => x.product === item.product)
 
-			if (existItem) {
-				return {
-					...state,
-					cartItems: state.cartItems.map((x) =>
-						x.product === existItem.product
-							? item
-							: x
-					),
-				}
-			} else {
-				return {
-					...state,
-					cartItems: [...state.cartItems, item],
-				}
-			}
-			break
+      if (existItem) {
+        return {
+          ...state,
+          cartItems: state.cartItems.map((x) =>
+            x.product === existItem.product ? item : x
+          ),
+        }
+      } else {
+        return {
+          ...state,
+          cartItems: [...state.cartItems, item],
+        }
+      }
+      break
 
-		default:
-			return state
-	}
+    default:
+      return state
+  }
 }
