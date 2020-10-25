a85295c3b2c331e603d9c7025d88a9d82c759889
Darbaz Ali <darbaz.me@gmail.com>
2020-10-25 11:49:28 +0300

    User Authenticatin Endpoint

--- backend/server.js
+++ backend/server.js
@@ -3,31 +3,41 @@ console.clear()
 global.log = console.log
 
 import express from 'express'
+import bodyParser from 'body-parser'
 import colors from 'colors'
 
 import { config } from 'dotenv'
 
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
+import userRoute from './routes/userRoute.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 connectDB()
 
 const app = express()
 config() // dotenv
+
+// middlewares
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 const { PORT, NODE_ENV } = process.env
 
 app.get('/', (req, res) => {
-    res.send('API is working..')
+  res.send('API is working..')
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoute)
 
 app.use(notFound)
 
 app.use(errorHandler)
 
 app.listen(
-    PORT,
-    log(`Server running in ${NODE_ENV} mode on port ${PORT}`.yellow.bold)
+  PORT,
+  log(`Server running in ${NODE_ENV} mode on port ${PORT}`.yellow.bold)
 )
