d504bbd74fce8a100742cf7fea58aced38058353
Darbaz Ali <darbaz.me@gmail.com>
2021-02-27 10:50:09 +0300

    React Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,24 @@
 import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+
 import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
-
-import { Container } from 'react-bootstrap'
+import ProductScreen from './screens/ProductScreen'
 
 const App = () => {
   return (
-    <div>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <HomeScreen />
+          <Route exact path='/' component={HomeScreen} />
+          <Route path='/product/:id' component={ProductScreen} />
         </Container>
       </main>
       <Footer />
-    </div>
+    </Router>
   )
 }
 
