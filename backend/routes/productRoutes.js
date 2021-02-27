f15bff1a53725b104da3d48db008142be638c416
Darbaz Ali <darbaz.me@gmail.com>
2021-02-27 15:45:50 +0300

    fetching products from database

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,34 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import asyncHandler from 'express-async-handler'
+import Product from '../models/productModel.js'
+
+const router = express.Router()
+
+// @desc    fetch all products
+// @route   GET /api/products
+// @access  public
+router.get(
+  '/',
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({})
+    res.status(200).json(products)
+  })
+)
+
+// @desc    fetch a single product
+// @route   GET /api/products/:id
+// @access  public
+router.get(
+  '/:id',
+  asyncHandler(async (req, res) => {
+    const { id } = req.params
+    if (!mongoose.isValidObjectId(id))
+      return res.status(400).json({ error: 'Not a valid id' })
+    const product = await Product.findById({ _id: id })
+    if (product) return res.status(200).json(product)
+    else return res.status(404).json({ error: 'product not found' })
+  })
+)
+
+export default router
--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,10 @@ console.clear()
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
-import products from './data/products.js'
+// import products from './data/products.js'
 import config from '../config/config.js'
 import connectDB from './lib/dbConnection.js'
+import productRoutes from './routes/productRoutes.js'
 
 const app = express()
 dotenv.config()
@@ -19,13 +20,8 @@ app.use(express.json())
 app.get('/', (req, res) => {
   res.send('API is running')
 })
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id == req.params.id)
-  res.json(product)
-})
+
+app.use('/api/products', productRoutes)
 
 app.listen(config.port, () => {
   console.log(
