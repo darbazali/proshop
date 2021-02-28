55aa41540718f65f29c51ae2269cef0e9ea80faa
Darbaz Ali <darbaz.me@gmail.com>
2021-02-28 11:01:49 +0300

    Redux state into HomeScreen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,11 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Col, Row } from 'react-bootstrap'
-// import products from '../products'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
 import Product from '../components/Product'
 import { listProducts } from '../actions/productActions'
+
 const HomeScreen = () => {
   const dispatch = useDispatch()
   const productList = useSelector((state) => state.productList)
@@ -16,9 +18,9 @@ const HomeScreen = () => {
     <>
       <h1>Latest Products</h1>
       {loading ? (
-        <h2>Loading...</h2>
+        <Loader />
       ) : error ? (
-        <h3>{error}</h3>
+        <Message variant='danger'>{error}</Message>
       ) : (
         <Row>
           {products.map((product) => (
