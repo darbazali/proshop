fe1b082e545806dc83969cea76318f5a4b95557a
Darbaz Ali <darbaz.me@gmail.com>
2021-03-06 14:51:40 +0300

    #frontend Logout & show user in Navbar

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -2,6 +2,7 @@ import {
   USER_LOGIN_FAIL,
   USER_LOGIN_SUCCESS,
   USER_LOGIN_REQUEST,
+  USER_LOGOUT,
 } from '../constants/userContstants'
 
 import axios from 'axios'
@@ -38,4 +39,9 @@ export const login = (email, password) => async (dispatch) => {
           : error.message,
     })
   }
+}
+
+export const logout = () => (dispatch) => {
+  localStorage.removeItem('userInfo')
+  dispatch({ type: USER_LOGOUT })
 }
