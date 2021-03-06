14d819e92f035c783fbf8e7d25d2c1f94e07fa52
Darbaz Ali <darbaz.me@gmail.com>
2021-03-06 12:50:55 +0300

    User registration

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -28,4 +28,12 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password)
 }
 
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next()
+  }
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+})
+
 export default mongoose.model('User', userSchema)
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,9 +3,8 @@ import userCtrl from '../controllers/userController.js'
 import protect from '../lib/authMiddleware.js'
 
 const router = express.Router()
-router.route('/').get(userCtrl.getUsers)
+router.route('/').get(userCtrl.getUsers).post(userCtrl.registerUser)
 router.post('/login', userCtrl.authUser)
 router.get('/profile', protect, userCtrl.getUserProfile)
-// router.route('/profile').get(protect, userCtrl.getUserProfile)
 
 export default router
