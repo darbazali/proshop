88d4e274c5c584c76556fd91d0d8b424c0193d88
Darbaz Ali <darbaz.me@gmail.com>
2021-03-07 13:46:38 +0300

    #backend Get Order by ID

--- backend/controllers/orderControllers.js
+++ backend/controllers/orderControllers.js
@@ -35,5 +35,18 @@ const addOrderItems = asyncHandler(async (req, res) => {
     res.status(201).json(createdOrder)
   }
 })
+// @desc     Create order by id
+// @route   GET /api/orders/:id
+// @access  private
+const getOrderById = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id)
 
-export { addOrderItems }
+  if (order) {
+    return res.status(200).json(order)
+  } else {
+    res.status(404)
+    throw new Error('Order not found')
+  }
+})
+
+export { addOrderItems, getOrderById }
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { addOrderItems } from '../controllers/orderControllers.js'
+import { addOrderItems, getOrderById } from '../controllers/orderControllers.js'
 import protect from '../lib/authMiddleware.js'
 
 const router = express.Router()
 
 router.route('/').post(protect, addOrderItems)
+router.route('/:id').get(protect, getOrderById)
 
 export default router
