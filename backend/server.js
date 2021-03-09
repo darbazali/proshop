6fd3d29525e5ba2f0e1952c3aaac8f1381d8256f
Darbaz Ali <darbaz.me@gmail.com>
2021-03-09 13:46:09 +0300

    prepare to deploy

--- backend/server.js
+++ backend/server.js
@@ -31,14 +31,21 @@ const __dirname = path.resolve()
 
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
-app.get('/', (req, res) => {
-  res.send('API is running')
-})
-
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
 app.use('/api/uploads', uploadRoutes)
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  })
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running')
+  })
+}
 
 // 404
 app.use(errorMiddleware.notFound)
