17ec8e4c0f2d8999d6a07d9c535d2af868217ce2
Darbaz Ali <darbaz.me@gmail.com>
2021-03-13 10:37:33 +0300

    React-Bootstrap, Header and Footer

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+const Footer = () => {
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col className='text-center'>&copy; 2021 - ProShop</Col>
+        </Row>
+      </Container>
+    </footer>
+  )
+}
+
+export default Footer
