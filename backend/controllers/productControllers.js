c12a1477b5cb2fdce48dcccf381d484843912d66
Darbaz Ali <darbaz.me@gmail.com>
2021-03-13 15:30:48 +0300

    #backend Custom Error Handler

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -9,7 +9,10 @@ import Product from '../models/productModel.js'
 const getProducts = asyncHandler(async (req, res) => {
   const products = await Product.find({})
   if (products) return res.status(200).json(products)
-  else return res.status(404).json({ error: 'No products found' })
+  else {
+    res.status(404)
+    throw new Error('No products found')
+  }
 })
 
 /*============================================================================
@@ -20,7 +23,10 @@ const getProducts = asyncHandler(async (req, res) => {
 const getProductById = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id)
   if (product) return res.status(200).json(product)
-  else return res.status(404).json({ error: 'Product not found' })
+  else {
+    res.status(404)
+    throw new Error('Product not found')
+  }
 })
 
 export default { getProducts, getProductById }
--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,11 @@ console.clear()
 
 import express from 'express'
 import dotenv from 'dotenv'
+import morgan from 'morgan'
 import colors from 'colors'
 import config from '../config/config.js'
 import connectDB from './lib/connectDB.js'
+import { notFound, errorHandler } from './lib/errorMiddleware.js'
 
 // routers
 import productRoutes from './routes/productRoutes.js'
@@ -16,11 +18,19 @@ connectDB()
 
 const app = express()
 
+if (config.env === 'development') {
+  app.use(morgan('dev'))
+}
+
 app.get('/', (req, res) => {
   res.send('Api is running')
 })
 
 app.use('/api/products', productRoutes)
+
+// Not found
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(config.port, () => {
   console.log(
