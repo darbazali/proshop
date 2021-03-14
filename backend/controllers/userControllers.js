b5e94da9ab13ae99f38b6591c0acac91ddfb57ca
Darbaz Ali <darbaz.me@gmail.com>
2021-03-14 17:36:15 +0300

    #backend Custom Auth Middleware

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -28,5 +28,24 @@ const authUser = asyncHandler(async (req, res) => {
     throw new Error('Invalid email or password')
   }
 })
+/*============================================================================
+@desc   Get User Profile
+@route  GET /api/users/profile
+@access Private
+=============================================================================*/
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
 
-export default { authUser }
+export default { authUser, getUserProfile }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,9 @@
 import express from 'express'
 import userCtrl from '../controllers/userControllers.js'
+import protect from '../lib/authMiddleware.js'
 const router = express.Router()
 
 router.route('/login').post(userCtrl.authUser)
+router.route('/profile').get(protect, userCtrl.getUserProfile)
 
 export default router
