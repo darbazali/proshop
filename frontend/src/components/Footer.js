9cbdb2dc9c93aa8eebfb8fc4ed4050022950ed0c
Darbaz Ali <darbaz.me@gmail.com>
2021-03-24 14:28:49 +0300

    React-Bootstrap Setup, Header & Footer

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+const Footer = () => {
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col className='text-center'>©2021 ProShop - by Darbaz Ali</Col>
+        </Row>
+      </Container>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Navbar, Nav, Container } from 'react-bootstrap'
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+          <Navbar.Brand href='/'>ProShop</Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ml-auto'>
+              <Nav.Link href='/cart'>Cart</Nav.Link>
+              <Nav.Link href='/login'>Sign In</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  )
+}
+
+export default Header
