0f25cbd5d28f0ee77315ac7991d2026826b47292
Darbaz Ali <darbaz.me@gmail.com>
2021-03-24 13:43:35 +0300

    rebuild

--- backend/routes/productRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import productCtrl from '../controllers/productControllers.js'
-const router = express.Router()
-
-router.route('/').get(productCtrl.getProducts)
-router.route('/:id').get(productCtrl.getProductById)
-
-export default router
--- config/config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-  env: process.env.NODE_ENV || 'development',
-  port: process.env.PORT || 5000,
-  mongoUri: process.env.MONGO_URI || 'mongodb://localhost:27017/proshop',
-  jwtSecret: process.env.JWT_SECRET || 'my_aweseom_secret',
-}
-
-export default config
