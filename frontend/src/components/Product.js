6d2d4cab9a67bd707dd694dda643c3c8c80b42a0
Darbaz Ali <darbaz.me@gmail.com>
2021-03-24 16:07:35 +0300

    Rating Component

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import Rating from './Rating'
 const Product = ({ product }) => {
   return (
     <Card className='my-3 p-3 rounded'>
@@ -14,11 +15,7 @@ const Product = ({ product }) => {
           </Card.Title>
         </a>
 
-        <Card.Text>
-          <div className='my-3'>
-            {product.rating} from {product.numReviews} reviews
-          </div>
-        </Card.Text>
+        <Rating value={product.rating} text={`${product.numReviews} reviews`} />
 
         <Card.Text as='h3'>${product.price}</Card.Text>
       </Card.Body>
