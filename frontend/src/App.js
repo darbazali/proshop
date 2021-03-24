332b574738abdf565a4324c0fe3bfc5a24b92eb2
Darbaz Ali <darbaz.me@gmail.com>
2021-03-24 16:42:26 +0300

    Product Details Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 
 import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
       <main className='py-3'>
         <Container>
           <Route path='/' component={HomeScreen} exact />
+          <Route path='/products/:id' component={ProductScreen} />
         </Container>
       </main>
       <Footer />
