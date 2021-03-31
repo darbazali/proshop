4d4df2cddb44488d49c26cc579575ff7c9c94ef9
Darbaz Ali <darbaz.me@gmail.com>
2021-03-31 05:58:49 +0300

    create & update product endpoint

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -41,5 +41,66 @@ const deleteProduct = asyncHandler(async (req, res) => {
     throw new Error('Product not found')
   }
 })
+/*=============================================================
+Create a Product
+POST /api/products/
+Private/Admin
+==============================================================*/
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: 'Sample name',
+    price: 0,
+    user: req.user._id,
+    image: '/images/sample.jpg',
+    brand: 'Sample brand',
+    category: 'Sample category',
+    countInStock: 0,
+    numReviews: 0,
+    description: 'Sample descreption',
+  })
 
-export default { getProducts, getProductByID, deleteProduct }
+  const createdProduct = await product.save()
+
+  res.json(createdProduct)
+})
+/*=============================================================
+Update a Product
+PUT /api/products/:id
+Private/Admin
+==============================================================*/
+const updateProduct = asyncHandler(async (req, res) => {
+  const {
+    name,
+    price,
+    description,
+    image,
+    brand,
+    category,
+    countInStock,
+  } = req.body
+
+  const product = await Product.findById(req.params.id)
+  if (product) {
+    product.name = name
+    product.price = price
+    product.description = description
+    product.image = image
+    product.brand = brand
+    product.category = category
+    product.countInStock = countInStock
+
+    const updatedProduct = await product.save()
+    res.json(updatedProduct)
+  } else {
+    res.status(404)
+    throw new Error('Product not found')
+  }
+})
+
+export default {
+  getProducts,
+  getProductByID,
+  deleteProduct,
+  createProduct,
+  updateProduct,
+}
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,11 +3,15 @@ import productCtrl from '../controllers/productControllers.js'
 import { protect, admin } from '../lib/authMiddleware.js'
 const router = express.Router()
 
-router.route('/').get(productCtrl.getProducts)
+router
+  .route('/')
+  .get(productCtrl.getProducts)
+  .post(protect, admin, productCtrl.createProduct)
 
 router
   .route('/:id')
   .get(productCtrl.getProductByID)
   .delete(protect, admin, productCtrl.deleteProduct)
+  .put(protect, admin, productCtrl.updateProduct)
 
 export default router
